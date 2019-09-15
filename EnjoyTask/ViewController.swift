c23759845249061df42bc15e3ea6021e8cd0ba49
RyoYamada <newfield1001@gmail.com>
2019-09-15 23:29:49 +0900

    Generate project and add gitignore file.

---
+++ EnjoyTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EnjoyTask
+//
+//  Created by 山田良 on 2019/09/15.
+//  Copyright © 2019 yamadaryo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
