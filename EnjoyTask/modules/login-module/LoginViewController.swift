16063c01fd57679d1e4ef829f77da5efb3159c26
RyoYamada <newfield1001@gmail.com>
2019-09-21 00:35:02 +0900

    ログイン画面の枠組みを作成

--- EnjoyTask/modules/login-module/LoginViewController.swift
+++ EnjoyTask/modules/login-module/LoginViewController.swift
@@ -9,5 +9,23 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    @IBOutlet weak var emailTextField: UITextField! {
+        didSet {
+            setPlaceholderLabelColor(emailTextField)
+        }
+    }
 
+    @IBOutlet weak var passwordTextField: UITextField! {
+        didSet {
+            setPlaceholderLabelColor(passwordTextField)
+        }
+    }
+}
+
+private extension LoginViewController {
+    func setPlaceholderLabelColor(_ textField: UITextField) {
+        let iVar = class_getInstanceVariable(UITextField.self, "_placeholderLabel")!
+        let placeholderLabel = object_getIvar(textField, iVar) as! UILabel
+        placeholderLabel.textColor = .white
+    }
 }
